d58371eb4032f72fce791b78efa23e65e1907499
tamara washington <tamey.washington@gmail.com>
2021-03-31 15:43:07 -0500

    final deplpyment

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 from numpy.random import f
 
-engine = create_engine('postgresql://postgres:password@happinesscoviddb.cy7ekxurfwul.us-east-2.rds.amazonaws.com:5432/postgres')
+engine = create_engine('postgresql://postgres:ricebootcamp@happinesscoviddb.cy7ekxurfwul.us-east-2.rds.amazonaws.com')
 
 app = Flask(__name__)
 
@@ -21,7 +21,7 @@ def home():
     return render_template("index.html")
 
 # App route countries
-# -----------------------------------------------------
+#-----------------------------------------------------
 @app.route("/countries")
 def countries():
     results=engine.execute("SELECT country FROM un_govt").fetchall()
@@ -31,11 +31,11 @@ def countries():
     return jsonify(country)
 
 
-# # App route to Home Page
-# #-----------------------------------------------------
+# App route to Home Page
+#-----------------------------------------------------
 @app.route("/demographics")
 def demographics():
-    results=engine.execute("""select  u.id, u.country, u.constitutional_form, u.population_2020, w.world_region, w.gdp_per_capita
+    results=engine.execute("""select  u.id, u.country, u.constitutional_form, u.population_2020, w.world_region, 
     from un_govt as u
     join world_happiness as w
     on (u.id=w.country_id)""").fetchall()
@@ -43,23 +43,23 @@ def demographics():
 
     for result in results:
         country_data={}
-        country_data["id"] = result[0]
-        country_data["country_name"] = result[1]
-        country_data["type_of_government"] = result[2]
-        country_data["population"] = result[3]
-        country_data["world_region"] = result[4]
-        country_data["gdp"] = result[5]
+        country_data["ID: "] = result[0]
+        country_data["Country Name: "] = result[1]
+        country_data["Type of Government: "] = result[2]
+        country_data["Population: "] = result[3]
+        country_data["World Region: "] = result[4]
+
         
         demographics.append(country_data)
 
     return jsonify(demographics)
 
-# # App route to map
-# #-----------------------------------------------------
+# App route to map
+#-----------------------------------------------------
 
 @app.route("/map.html")
 def map():
-    results = engine.execute("""select u.country, w.happiness_score,  u.latitude, u.longitude,sum(c.new_deaths) as "total_new_deaths", sum(c.new_cases) as "total_new_cases"
+    results = engine.execute("""select u.id,u.country, w.happiness_score,  u.latitude, u.longitude,sum(c.new_deaths) as "total_new_deaths", sum(c.new_cases) as "total_new_cases"
     from un_govt as u
 	inner join world_happiness as w
 	on (u.id=w.country_id)
@@ -71,12 +71,13 @@ def map():
     happiness_vs_covid = []
     for result in results:
         covid_happiness={}
-        covid_happiness["country"] = result[0]
-        covid_happiness["happiness_score"] = result[1]
-        covid_happiness["latitude"] = result[2]
-        covid_happiness["longitude"] = result[3]
-        covid_happiness["total_new_deaths"] = result[4]
-        covid_happiness["total_new_cases"] = result[5]
+        covid_happiness["id"]= result[0]
+        covid_happiness["country"] = result[1]
+        covid_happiness["happiness_score"] = result[2]
+        covid_happiness["latitude"] = result[3]
+        covid_happiness["longitude"] = result[4]
+        covid_happiness["total_new_deaths"] = result[6]
+        covid_happiness["total_new_cases"] = result[6]
 
 
         happiness_vs_covid.append(covid_happiness)
@@ -91,7 +92,7 @@ def map():
 
 @app.route("/demographics/<country>")
 def country_demographic(country):
-    results=engine.execute(f"""select  u.id, u.country, u.constitutional_form, u.population_2020, w.world_region, w.gdp_per_capita
+    results=engine.execute(f"""select  u.id, u.country, u.constitutional_form, u.population_2020, w.world_region
     from un_govt as u
     join world_happiness as w
     on (u.id=w.country_id) where u.country= '{country}'""").fetchall()
@@ -99,47 +100,77 @@ def country_demographic(country):
 
     for result in results:
         country_data={}
-        country_data["id"] = result[0]
-        country_data["country_name"] = result[1]
-        country_data["type_of_government"] = result[2]
-        country_data["population"] = result[3]
-        country_data["world_region"] = result[4]
-        country_data["gdp"] = result[5]
+        country_data["ID"] = result[0]
+        country_data["Country Name"] = result[1]
+        country_data["Type of Government"] = result[2]
+        country_data["Population"] = result[3]
+        country_data["World Region"] = result[4]
+
         
 
 
     return jsonify(country_data)
     
 # App route to demographics for happiness vs covid data
+
+#-----------------------------------------------------
+
+@app.route("/happiness_vs_covid")
+def happiness_vs_covid():
+    results = engine.execute("""select u.country, u.population_2020, w.happiness_score,  u.latitude, u.longitude,sum(c.new_deaths) as "total_new_deaths", sum(c.new_cases) as "total_new_cases"
+    from un_govt as u
+	inner join world_happiness as w
+	on (u.id=w.country_id)
+	inner join world_covid_data as c
+	on (w.country_id = c.country_id)
+	group by u.id, w.happiness_score
+	order by u.country """).fetchall()
+    
+    happiness_vs_covid = []
+    for result in results:
+        covid_happiness={}
+        covid_happiness["country"] = result[0]
+        covid_happiness["population_2020"] = result[1]
+        covid_happiness["happiness_score"] = result[2]
+        covid_happiness["latitude"] = result[3]
+        covid_happiness["longitude"] = result[4]
+        covid_happiness["total_new_deaths"] = result[5]
+        covid_happiness["total_new_cases"] = result[6]
+
+
+        happiness_vs_covid.append(covid_happiness)
+
+    return jsonify(happiness_vs_covid)
+
+# App route to demographics for happiness vs covid data
 # grabbing one country at a time
 #-----------------------------------------------------
 
-# @app.route("/happiness_vs_covid")
-# def happiness_vs_covid():
-#     results = engine.execute("""select u.country, w.happiness_score,  u.latitude, u.longitude,sum(c.new_deaths) as "total_new_deaths", sum(c.new_cases) as "total_new_cases"
-#     from un_govt as u
-# 	inner join world_happiness as w
-# 	on (u.id=w.country_id)
-# 	inner join world_covid_data as c
-# 	on (w.country_id = c.country_id)
-# 	group by u.id, w.happiness_score
-# 	order by u.country """).fetchall()
+@app.route("/happiness_vs_covid_charts/<country>")
+def happiness_vs_covid_charts(country):
     
-#     happiness_vs_covid = []
-#     for result in results:
-#         covid_happiness={}
-#         covid_happiness["country"] = result[0]
-#         covid_happiness["happiness_score"] = result[1]
-#         covid_happiness["latitude"] = result[2]
-#         covid_happiness["longitude"] = result[3]
-#         covid_happiness["total_new_deaths"] = result[4]
-#         covid_happiness["total_new_cases"] = result[5]
-
-
-#         happiness_vs_covid.append(covid_happiness)
-
-#     return jsonify(happiness_vs_covid)
-
+    results = engine.execute(f"""select u.id, u.country, w.new_cases, w.new_deaths, wh.happiness_score
+    from un_govt as u
+	inner join world_covid_data as w
+	on (u.id=w.country_id) inner join world_happiness as wh
+	on (w.country_id = wh.country_id) where u.country = '{country}' 
+	group by u.id, w.new_cases, w.new_deaths, wh.happiness_score
+	order by w.new_cases desc, w.new_deaths desc
+	limit 10; """).fetchall()
+    
+    hap_vs_covid_charts=[]
+    for result in results:
+        select_country={}
+        select_country["id"] = result[0]
+        select_country["country"] = result[1]
+        select_country["new_cases"] = result[2]
+        select_country["new_deaths"] = result[3]
+        select_country["happiness_score"] = result[4]
+        
+        hap_vs_covid_charts.append(select_country)
+    
+    return jsonify(results)
+    happiness_vs_covid.to_json('static/js/happiness_vs_covid.json')
 # App route to demographics for government response data
 # grabbing one country at a time
 #-----------------------------------------------------
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 // Step 1 Populate Drop Down
 // Function to Collect names of countries names to Section tag with options using id selDataset 
+//--------------------------------------------
+//--------------------------------------------
 function init() {
     // Use a list of names from names
     d3.json("/countries").then((data) => {
@@ -12,15 +14,12 @@ function init() {
                 .property("value", country); 
 
         });
-        // Use the first sample from the list to build the initial plots
-        // have sample1 as a constant variable to keep when initiated
-        // const sample1 = sampleNames[0];
-        // console.log("sample1",sample1);
-        // buildCharts(sample1);
-        // buildMetadata(sample1);
+
     });
 }
-
+// Step 2 Define option changed
+//--------------------------------------------
+//--------------------------------------------
 // Function to change data each time to update charts
 function optionChanged(country) {
     // Fetch new data each time a new sample is selected
@@ -29,20 +28,109 @@ function optionChanged(country) {
 }
 init();
 
-// Step 2 Define option changed
-
 // Step 3 Demographic
+//--------------------------------------------
+//--------------------------------------------
 function demographicInfo(country) {
     d3.json(`/demographics/${country}`).then((data) => {
-        
+
         let PANEL = d3.select("#sample-metadata");
         // empty the panel in html before appending key, value pairs
         PANEL.html("");
         Object.entries(data).forEach(([key, value]) => {
-            d3.select("#sample-metadata").append("h4").text(`${key}: ${value}`);
+            d3.select("#sample-metadata").append("h4").text(`${key}:   ${value}`);
         });
     });
 }
 // STep 4 bubble chart
+//--------------------------------------------
+//--------------------------------------------
+//--find in bubbleapp.js----------------------
 
 // Step 5 map
+//--------------------------------------------
+//--------------------------------------------
+d3.json("/happiness_vs_covid").then((happinessDataSample) => {
+
+    
+    function happyMarkerSize(happiness_score) {
+        return happiness_score * 5000;
+    }
+
+    function deathMarkerSize(sumOfNewDeaths) {
+        return sumOfNewDeaths * 50;
+    }
+
+    let streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoia3A3Mi1oZWxsbyIsImEiOiJja203eDNtbjkwZGZ0MnVxZm1yOGRlYnJzIn0.fGY0KKU9o44jdyhZtdCbog'
+    });
+
+
+    // Create two separate layer groups for death and happiness score
+    var deaths = L.layerGroup();
+    var score = L.layerGroup();
+
+    //overlay object
+    let overlayMaps = {
+        "No of Deaths": deaths,
+        "Happiness Score": score
+    };
+
+    //Base map object
+    let baseMaps = {
+        "Grey Layer": streetmap
+    };
+
+    //Create the map
+    let myMap = L.map("map", {
+        center: [40.573883587770304, -3.873485396773201],
+        zoom: 2.5,
+        layers: [streetmap, score, deaths] 
+    });
+
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
+    for (var i = 0; i < happinessDataSample.length; i++) {
+            
+            var happinessRating = parseFloat((happinessDataSample[i]['happiness_score'])).toFixed(2)
+            console.log(happinessRating);
+            console.log(happinessDataSample[i].latitude);
+            
+            L.circle([happinessDataSample[i].latitude, happinessDataSample[i].longitude], {
+                stroke: true,
+                color: "green",
+                weight: 3,
+                innerfill: true,
+                radius: happyMarkerSize((happinessRating)*10)
+            // }).bindPopup(`<h1> ${happinessDataSample[i].latitude}, ${happinessDataSample[i].longitude}</h1><br>
+            // <h1> Happiness Score: ${happinessRating}</h1>`)
+            // .addTo(score)
+            }).bindPopup(`<h1>${happinessDataSample[i].country} </h1><br>
+            <h1> Happiness Score: ${happinessRating}</h1>`)
+            .addTo(score)
+        }
+
+    for (var i = 0; i < happinessDataSample.length; i++) {
+        
+        var numDeaths = Math.round(happinessDataSample[i]['total_new_deaths'])
+
+        L.circle([happinessDataSample[i].latitude, happinessDataSample[i].longitude], {
+            stroke: true,
+            color: "black",
+            height: 50,
+            weight: 0,
+            radius: deathMarkerSize((numDeaths)/3.5)
+        }).bindPopup(`<h1> ${happinessDataSample[i].country}</h1><br>
+        <h1> Number of Deaths: ${numDeaths}</h1>`)
+        .addTo(deaths)
+        }
+
+});
